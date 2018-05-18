dc9b6cb5565118a558a46412e6c3ba8a5ebae1a7
yangqiang1 <755689658@qq.com>
2018-05-18 23:21:50 +0800

    第一次提交

---
+++ FishDemo/Assets/Scripts/Flock.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public globalFlock myManager;
+    public float speed = 0.001f;
+    //旋转速度
+    float rotationSpeed = 4.0f;
+    Vector3 averageHeading;
+    Vector3 averagePosition;
+    //鱼之间的距离
+    float neighbourDistance = 3.0f;
+    //鱼的转向
+    bool turning = false;
+
+    public float speedMult = 1;
+
+    void Start()
+    {
+        //速度随机生成
+        speed = Random.Range(0.5f, 1);
+    }
+
+   
+    void Update()
+    {
+        //获得边界框
+        Bounds b = new Bounds(myManager.transform.position, myManager.swimLimits * 2);
+        //判断鱼的活动范围
+        //if(Vector3.Distance(transform.position,Vector3.zero)>=globalFlock.tankSize)
+        if(!b.Contains(transform.position))
+        {
+            turning = true;
+        }
+        else
+        {
+            turning = false;
+        }
+        if(turning)
+        {
+            Vector3 direction = myManager.transform.position- transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(direction),
+                rotationSpeed * Time.deltaTime);
+            speed = Random.Range(0.5f, 1)*speedMult;
+        }
+        else
+        {
+            if (Random.Range(0, 5) < 1)
+                ApplyRules();
+        }
+        transform.Translate(0, 0, Time.deltaTime * speedMult);
+    }
+
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = globalFlock.allFish;
+
+        Vector3 vcentre = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        //团队速度
+        float gSpeed = 0.1f;
+
+        Vector3 goalPos = myManager.goalPos;
+
+        float dist;
+
+        int groupSize = 0;
+        foreach (GameObject go in gos)
+        {
+            if (go != this.gameObject)
+            {
+                dist = Vector3.Distance(go.transform.position, this.transform.position);
+                if (dist <= neighbourDistance)
+                {
+                    vcentre += go.transform.position;
+                    groupSize++;
+                    if (dist < 1.0f)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+
+                    }
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+            }
+        }
+        //鱼群大小大于0
+        if (groupSize > 0)
+        {
+            //计算平均群的中心以及速度
+            vcentre = vcentre / groupSize + (goalPos - this.transform.position);
+            speed = gSpeed / groupSize*speedMult;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+            if (direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                    Quaternion.LookRotation(direction),
+                    rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ FishDemo/Assets/Scripts/globalFlock.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalFlock : MonoBehaviour
+{
+    public globalFlock myFlock;
+    public GameObject fishPrefab;
+
+    static int numFish = 20;
+    //声明鱼群数组
+    public static GameObject[] allFish = new GameObject[numFish];
+    public Vector3 goalPos;
+    //鱼群活动范围
+    public Vector3 swimLimits = new Vector3(2, 2, 2);
+    //生成范围
+    //public static int tankSize = 1;
+    //目标位置
+    //public static Vector3 goalPos = Vector3.zero;
+
+    public void FishSpeed(float speedMult)
+    {
+        Debug.Log(speedMult);
+        for(int i = 0; i < numFish; i++)
+        {
+            allFish[i].GetComponent<Flock>().speedMult = speedMult;
+        }
+    }
+    //限制鱼群游动的区域
+    void  OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0, 0, 0.5f, 0.5f);
+        Gizmos.DrawCube(transform.position, new Vector3(swimLimits.x*2,swimLimits.y*2,swimLimits.z*2));
+        Gizmos.color = new Color(1,1,1,1);
+        Gizmos.DrawSphere(goalPos, 0.1f);
+
+    }
+    void Start()
+    {
+        myFlock = this;
+        goalPos = this.transform.position;
+        RenderSettings.fogColor = Camera.main.backgroundColor;
+        RenderSettings.fogDensity = 0.03f;
+        RenderSettings.fog = true;
+        //通过遍历鱼随机设置他们的位置
+        for (int i = 0; i < numFish; i++)
+        {
+            //位置
+            Vector3 pos = new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                Random.Range(-swimLimits.y, swimLimits.y),
+                Random.Range(-swimLimits.z, swimLimits.z));
+            //Vector3 pos = new Vector3(1.0f,1.2f,1.3f);
+            //一 预设 二实例化预设坐标 三实例化预设角度
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
+            allFish[i].GetComponent<Flock>().myManager = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 1000) < 50)
+        {
+            goalPos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                                                  Random.Range(-swimLimits.y, swimLimits.y),
+                                                   Random.Range(-swimLimits.z, swimLimits.z));
+        }
+    }
+}
+
